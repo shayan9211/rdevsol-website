c4bd1f44fe063b070e56df5ba4e8a425564ad755
Shayan Roshan <shayan.roshan@gsa.gov>
2023-08-20 17:53:19 -0500

    added flask backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def hello_world():
-    return jsonify(message='Hello, World!!')
+def index():
+    return "Flask Backend is Running!"
 
 if __name__ == '__main__':
     app.run(debug=True)
